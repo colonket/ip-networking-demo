6fc6a97b5cbf37cc54077052c4865db219af22ed
Eric Iniguez <43428465+colonket@users.noreply.github.com>
2024-08-07 16:13:05 -0500

    Testing if editing typescript will properly update code

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 def generateHexString(string_length: number):
     global hexadecimal_characters, hex_string, random_char
-    hexadecimal_characters = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+    hexadecimal_characters = ["0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f"]
     hex_string = ""
     for index in range(string_length):
         random_char = hexadecimal_characters._pick_random()
-        hex_string = hex_string + random_char
+        hex_string = "" + hex_string + random_char
     return hex_string
 
 def on_received_string(receivedString):
     global split_packet
     split_packet = receivedString.split("~")
-    serial.write_line("" + hardware_address2 + " Received Size: " + str(len(split_packet)))
+    serial.write_line("" + hardware_address2 + " Received Size: " + ("" + str(len(split_packet))))
     while len(split_packet) > 0:
         serial.write_line("" + hardware_address2 + " received " + split_packet.shift())
     serial.write_line("")
@@ -21,11 +36,11 @@ def sendPacket(hardware_address: str, source_ip: str, destination_ip: str, proto
     packet = "" + hardware_address + "~" + source_ip + "~" + destination_ip + "~" + protocol + "~" + data
     radio.send_string(packet)
 packet = ""
-split_packet: List[str] = []
 random_char = ""
 hex_string = ""
 hexadecimal_characters: List[str] = []
 hardware_address2 = ""
+split_packet: List[str] = []
 radio.set_group(1)
 hardware_address2 = generateHexString(8)
 serial.write_line("Set MAC: " + hardware_address2)
--- main.ts
+++ main.ts
@@ -1,16 +1,41 @@
-function generateHexString(string_length: number): string {
-    
-    hexadecimal_characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+let msg_timestamp = 0
+let packet_pieces: string[] = []
+let packet = ""
+let rebuilt_packet = ""
+let random_char = ""
+let hex_string = ""
+let hexadecimal_characters: string[] = []
+let hardware_address2 = ""
+let split_packet: string[] = []
+
+
+
+
+function main () {
+    radio.setGroup(1)
+    hardware_address2 = generateHexString(8)
+    serial.writeLine("MAC: " + hardware_address2)
+    sendPacket(
+        hardware_address2,
+        "10.10.1.101",
+        "10.10.1.102",
+        "text",
+        "can you hear me? it's me margaret"
+    )
+    serial.writeLine(convertToText(input.runningTimeMicros()).substr(0, 4))
+}
+
+function generateHexString (string_length: number) {
+    hexadecimal_characters = "0123456789ABCDEF".split()
     hex_string = ""
     for (let index = 0; index < string_length; index++) {
         random_char = hexadecimal_characters._pickRandom()
-        hex_string = hex_string + random_char
+        hex_string = "" + hex_string + random_char
     }
     return hex_string
 }
-
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    
+radio.onReceivedString(function (receivedString) {
+    rebuilt_packet = ""
     split_packet = _py.py_string_split(receivedString, "~")
     serial.writeLine("" + hardware_address2 + " Received Size: " + ("" + split_packet.length))
     while (split_packet.length > 0) {
@@ -18,23 +43,20 @@ radio.onReceivedString(function on_received_string(receivedString: string) {
     }
     serial.writeLine("")
 })
-function sendPacket(hardware_address: string, source_ip: string, destination_ip: string, protocol: string, data: string) {
-    
+function sendPacket (hardware_address: string, source_ip: string, destination_ip: string, protocol: string, data: string) {
+    serial.writeLine("" + (control.eventTimestamp()))
     packet = "" + hardware_address + "~" + source_ip + "~" + destination_ip + "~" + protocol + "~" + data
-    radio.sendString(packet)
+    packet_pieces = [
+    hardware_address,
+    source_ip,
+    destination_ip,
+    protocol,
+    data
+    ]
+    msg_timestamp = input.runningTimeMicros() % 100
+    for (let value of packet_pieces) {
+        radio.sendString("" + msg_timestamp + "/" + value)
+    }
 }
 
-let packet = ""
-let split_packet : string[] = []
-let random_char = ""
-let hex_string = ""
-let hexadecimal_characters : string[] = []
-let hardware_address2 = ""
-radio.setGroup(1)
-hardware_address2 = generateHexString(8)
-serial.writeLine("Set MAC: " + hardware_address2)
-serial.writeLine("")
-sendPacket(hardware_address2, "10.10.1.101", "10.10.1.102", "text", "hello world")
-basic.forever(function on_forever() {
-    
-})
+main()
