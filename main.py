f7edebcc8f203f05e73a67ec482e5d7deaae970c
Eric Iniguez <43428465+colonket@users.noreply.github.com>
2024-08-15 08:26:30 -0500

    Implemented packet splitting

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 def generateHexString(string_length: number):
     global hexadecimal_characters, hex_string, random_char
-    hexadecimal_characters = ["0",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f"]
+    hexadecimal_characters = "0123456789ABCDEF".split("")
     hex_string = ""
     for index in range(string_length):
         random_char = hexadecimal_characters._pick_random()
@@ -23,7 +8,8 @@ def generateHexString(string_length: number):
     return hex_string
 
 def on_received_string(receivedString):
-    global split_packet
+    global rebuilt_packet, split_packet
+    rebuilt_packet = ""
     split_packet = receivedString.split("~")
     serial.write_line("" + hardware_address2 + " Received Size: " + ("" + str(len(split_packet))))
     while len(split_packet) > 0:
@@ -32,25 +18,31 @@ def on_received_string(receivedString):
 radio.on_received_string(on_received_string)
 
 def sendPacket(hardware_address: str, source_ip: str, destination_ip: str, protocol: str, data: str):
-    global packet
+    global packet, packet_pieces, msg_timestamp
+    serial.write_line("" + str((control.event_timestamp())))
     packet = "" + hardware_address + "~" + source_ip + "~" + destination_ip + "~" + protocol + "~" + data
-    radio.send_string(packet)
+    packet_pieces = [hardware_address, source_ip, destination_ip, protocol, data]
+    msg_timestamp = input.running_time_micros() % 100
+    for value in packet_pieces:
+        radio.send_string("" + str(msg_timestamp) + "/" + value)
+def main():
+    global hardware_address2
+    radio.set_group(1)
+    hardware_address2 = generateHexString(8)
+    serial.write_line("MAC: " + hardware_address2)
+    sendPacket(hardware_address2,
+        "10.10.1.101",
+        "10.10.1.102",
+        "text",
+        "can you hear me? it's me margaret")
+    serial.write_line(convert_to_text(input.running_time_micros()).substr(0, 4))
+msg_timestamp = 0
+packet_pieces: List[str] = []
 packet = ""
+hardware_address2 = ""
+rebuilt_packet = ""
 random_char = ""
 hex_string = ""
 hexadecimal_characters: List[str] = []
-hardware_address2 = ""
 split_packet: List[str] = []
-radio.set_group(1)
-hardware_address2 = generateHexString(8)
-serial.write_line("Set MAC: " + hardware_address2)
-serial.write_line("")
-sendPacket(hardware_address2,
-    "10.10.1.101",
-    "10.10.1.102",
-    "text",
-    "hello world")
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+main()
--- main.ts
+++ main.ts
@@ -1,32 +1,28 @@
-let msg_timestamp = 0
-let packet_pieces: string[] = []
-let packet = ""
-let rebuilt_packet = ""
-let random_char = ""
-let hex_string = ""
-let hexadecimal_characters: string[] = []
-let hardware_address2 = ""
-let split_packet: string[] = []
-
-
-
-
-function main () {
-    radio.setGroup(1)
-    hardware_address2 = generateHexString(8)
-    serial.writeLine("MAC: " + hardware_address2)
-    sendPacket(
-        hardware_address2,
-        "10.10.1.101",
-        "10.10.1.102",
-        "text",
-        "can you hear me? it's me margaret"
-    )
-    serial.writeLine(convertToText(input.runningTimeMicros()).substr(0, 4))
+input.onButtonPressed(Button.A, function () {
+    serial.writeLine("")
+    serial.writeLine("")
+    serial.writeLine("")
+    sendPacket(hardware_address, "10.10.1.101", "10.10.1.102", "text", "Hello world! It's me! Margaret! Boy this message sure is long")
+})
+function sendPacketFragment (time_stamp: number, field: string, packet_fragment: string) {
+    comm_buffer = ""
+    field_count = 0
+    iterator = 1
+    for (let value of packet_fragment) {
+        comm_buffer = "" + comm_buffer + value
+        if (iterator % 14 == 0) {
+            radio.sendString("" + time_stamp + field + field_count + comm_buffer)
+            comm_buffer = ""
+            field_count += 1
+        }
+        iterator += 1
+    }
+    if (comm_buffer != "") {
+        radio.sendString("" + time_stamp + field + field_count + comm_buffer)
+    }
 }
-
 function generateHexString (string_length: number) {
-    hexadecimal_characters = "0123456789ABCDEF".split()
+    hexadecimal_characters = "0123456789ABCDEF".split("")
     hex_string = ""
     for (let index = 0; index < string_length; index++) {
         random_char = hexadecimal_characters._pickRandom()
@@ -35,28 +31,32 @@ function generateHexString (string_length: number) {
     return hex_string
 }
 radio.onReceivedString(function (receivedString) {
-    rebuilt_packet = ""
-    split_packet = _py.py_string_split(receivedString, "~")
-    serial.writeLine("" + hardware_address2 + " Received Size: " + ("" + split_packet.length))
-    while (split_packet.length > 0) {
-        serial.writeLine("" + hardware_address2 + " received " + split_packet.shift())
-    }
-    serial.writeLine("")
+    serial.writeLine(receivedString)
 })
-function sendPacket (hardware_address: string, source_ip: string, destination_ip: string, protocol: string, data: string) {
-    serial.writeLine("" + (control.eventTimestamp()))
-    packet = "" + hardware_address + "~" + source_ip + "~" + destination_ip + "~" + protocol + "~" + data
-    packet_pieces = [
-    hardware_address,
-    source_ip,
-    destination_ip,
-    protocol,
-    data
-    ]
+function sendPacket (hardware_address: string, source_ip: string, destination_ip: string, protocol: string, content: string) {
+    serial.writeLine("[" + control.eventTimestamp() + "]" + hardware_address + "sent a packet!")
     msg_timestamp = input.runningTimeMicros() % 100
-    for (let value of packet_pieces) {
-        radio.sendString("" + msg_timestamp + "/" + value)
-    }
+    sendPacketFragment(msg_timestamp, "M", hardware_address)
+    sendPacketFragment(msg_timestamp, "S", source_ip)
+    sendPacketFragment(msg_timestamp, "D", destination_ip)
+    sendPacketFragment(msg_timestamp, "P", protocol)
+    sendPacketFragment(msg_timestamp, "C", content)
 }
-
+function main () {
+    radio.setGroup(1)
+    hardware_address = generateHexString(8)
+    serial.writeLine("Generated new MAC: " + hardware_address)
+}
+let msg_timestamp = 0
+let random_char = ""
+let hex_string = ""
+let hexadecimal_characters: string[] = []
+let iterator = 0
+let field_count = 0
+let comm_buffer = ""
+let hardware_address = ""
+let split_packet: string[] = []
 main()
+basic.forever(function () {
+    basic.showString(hardware_address)
+})
